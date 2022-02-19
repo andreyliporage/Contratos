126d9727e3f5642fb8ac1b468c39c52af97fd13c
andreyliporage <andreyliporage@gmail.com>
2022-02-19 15:40:13 -0300

    feature: constantes de taxa criadas em PayPalService

--- Contratos/Services/PayPalService.cs
+++ Contratos/Services/PayPalService.cs
@@ -2,12 +2,14 @@
 {
     internal class PayPalService : ITaxaService
     {
+        private const double taxaPorParcela = 0.01;
+        private const double taxaPorPagamento = 0.02;
 
         public double Taxa(double valorParcela, int numeroParcela)
         {
-            double taxaPorMes = valorParcela * 0.01 * numeroParcela;
+            double taxaPorMes = valorParcela * taxaPorParcela * numeroParcela;
             double valorComtaxaPorMes = valorParcela + taxaPorMes;
-            double valorComTaxaPorPagamento = valorComtaxaPorMes + (valorComtaxaPorMes * 0.02);
+            double valorComTaxaPorPagamento = valorComtaxaPorMes + (valorComtaxaPorMes * taxaPorPagamento);
 
             return valorComTaxaPorPagamento;
         }
